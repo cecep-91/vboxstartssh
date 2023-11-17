cba2d2e33463ab201083244fc18acf4e1bb5947d
Iqbal Rizky Wijaya <148958846+cecep-91@users.noreply.github.com>
2023-11-17 12:22:00 -0500

    kyaa~

--- start.sh
+++ start.sh
@@ -1,18 +1,45 @@
 #!/bin/bash
 
-# Get the VM's name and start it
+wait_boot() {
+    if [ $? -eq 0 ]
+    then
+        echo "Wait till the VM booted successfully"
+        sleep 30
+    fi
+}
+
+dossh() {
+    echo "Connecting to the VM via SSH"
+    while true
+    do
+        ssh ${UNAME}@${VM_IP}
+        sleep 3
+        if [ $? -eq 0 ]
+        then
+            exit 0
+        fi
+    done
+}
+
+# Get the variable needed
+SCRIPT_NAME=$(basename "$0")
+# Check syntax, set UNAME as root if not declared
+    if [ $# -lt 2 ] || [ $# -gt 3 ]; then
+        echo "usage: $SCRIPT_NAME <vm_name> <vm_ip> <username to ssh (optional)>"
+        exit 1
+    elif [ $# -eq 2 ]; then
+        UNAME="root"   
+    elif [ $# -eq 3 ]; then
+        UNAME="$3"
+    fi
 VM_NAME=$1
 VM_IP=$2
+
+# Start VM in background
 VBoxManage startvm "$VM_NAME" --type headless
 
 # Wait the VM to booted up
-echo "Wait till the VM booted successfully"
-sleep 30
+wait_boot
 
 # Try connecting SSH
-echo "Connecting to the VM via SSH"
-while true
-do
-    ssh root@$VM_IP
-    sleep 3
-done
+dossh
