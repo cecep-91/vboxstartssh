44073d3cd48172e9f6fba79e8a6fbe1dea06864b
Iqbal Rizky Wijaya <148958846+cecep-91@users.noreply.github.com>
2023-11-17 10:56:26 -0500

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the VM's name and start it
+VM_NAME=$1
+VM_IP=$2
+VBoxManage startvm "$VM_NAME" --type headless
+
+# Wait the VM to booted up
+echo "Wait till the VM booted successfully"
+sleep 30
+
+# Try connecting SSH
+echo "Connecting to the VM via SSH"
+while True
+do
+    ssh root@$VM_IP
+    sleep 3
+done
